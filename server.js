476eca393a172d1f7308c5b2ae5932304ea805ab
Focrult <focrult@gmail.com>
2023-02-08 21:36:12 +0800

    Fixed the routes between HTML pages!

--- server.js
+++ server.js
@@ -7,4 +7,12 @@ const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 console.log(`listening on port ${PORT}`);
-})
+});
+
+//fixing get betweem htmls
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
